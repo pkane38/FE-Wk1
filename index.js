02df75ee0f4b725216492c7bf59fd19c8ed4576a
Patrick <pkane38@gmail.com>
2021-03-30 19:18:36 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+/*Instructions: In VS Code, or an IDE of your choice, write the code that accomplishes the objectives listed below. 
+Ensure that the code compiles and runs as directed. Take screenshots of the code and of the running program (make sure to get screenshots 
+of all required functionality) and paste them in this document where instructed below. Create a new repository on GitHub for this week’s assignments and 
+push this document, with your JavaScript project code, to the repository. Lastly, in the Learning Management System, click the “Add Submission” button 
+and paste the URL to your GitHub repository. 
+*/
+var itemPrice = 10.00;
+var amountInWallet = 22.00;
+var numberOfFriends = 4;
+var ageInYears = 55;
+var firstName = "Patrick";
+var lastName = "Kane";
+var middleInitial ="W";
+
+
+console.log("I have $" + (amountInWallet - itemPrice) + " left after my purchase." );
+console.log("The average number of friends per year that I have " + (numberOfFriends / ageInYears));
+console.log("My full name is " + firstName + " " + middleInitial + " " + lastName);
